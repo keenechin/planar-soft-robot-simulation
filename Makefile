c2efddaf5ded3ebaee9b5dd6ecb4e02263a95fb9
Keene Chin <keene.chin@gmail.com>
2020-09-21 15:39:48 -0400

    libray window code added, properly linking to libray dynamic library

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 simulate: simulate.o
-	$(info Linking $<.)
-	g++ -o $@ $@.o
+	$(info Linking $<)
+	g++ -o $@ $@.o /usr/local/lib/libraylib.so
 %.o: %.cpp
-	$(info Compiling $<.)
+	$(info Compiling $<)
 	g++ -std=c++17 -c $<
 clean: 
 	$(info Deleting object files and executables.)
--- simulate.cpp
+++ simulate.cpp
@@ -8,6 +8,19 @@ using std::cin;
 
 int main(int argc, char *argv[]){
 
+	int screenWidth = 640;
+	int screenHeight = 480;
+	InitWindow(screenWidth, screenHeight, "simulation window");
+	SetTargetFPS(60);
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(BLACK);
+		DrawText("Simulation output goes here.", 190, 200, 20, LIGHTGRAY);
+		EndDrawing();
+	}
+
+	CloseWindow();
 
 	return 0;	
 }
